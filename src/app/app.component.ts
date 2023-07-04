ba543af33f1cf13130c2bbffa8eceaf14aba125b
DariusJ268 <darius.juralevic@gmail.com>
2023-07-04 14:42:48 +0300

    1st assignemnt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector:'.app-servers',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  //   h1 {
+  //   color: blue;
+  //   }
+  // `]
 })
 export class AppComponent {
 }
